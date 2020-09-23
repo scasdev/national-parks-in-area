5559a375f4d53c34698c7c1b3bfa08d6915c20d4
Stephan Salvant <stephansalvant@gmail.com>
2020-09-22 22:15:23 -0500

    final version v2

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-//api key
-// i6jg7Su8c8OSzFffVbcix8OC2dZwT4ZKHyFaEwZP 
-// curl -X GET "https://developer.nps.gov/api/v1/parks?stateCode=fl&limit=1&api_key=i6jg7Su8c8OSzFffVbcix8OC2dZwT4ZKHyFaEwZP" -H "accept: application/json"
-
-
-
-/*
-Requirements:
-//The user must be able to search for parks in one or more states.
-
-//The user must be able to set the max number of results, with a default of 10.
-
-//The search must trigger a call to NPS's API.
-
-The parks in the given state must be displayed on the page. 
-Include at least:
-Full name
-Description
-Website URL
-
-The user must be able to make multiple searches and see only the results for the current search.
-*/
-
 'use strict';
 
 const apiKey = 'i6jg7Su8c8OSzFffVbcix8OC2dZwT4ZKHyFaEwZP'; 
